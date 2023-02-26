117fa7a04bbf2534f62f6e9c8b451540b6f1bf65
Katie Huynh <htmdung1011@gmail.com>
2023-02-25 16:47:30 -0800

    complete first_three last_five middle inner_four inner_four_end replace_head replace_third_and_last replace_middle delete_third_and_seventh delete_middle functions

--- list_operations.py
+++ list_operations.py
@@ -62,7 +62,7 @@ def first_three(input_list):
 
     """
 
-    return []
+    return input_list[:3]
 
 
 def last_five(input_list):
@@ -75,7 +75,7 @@ def last_five(input_list):
 
     """
 
-    return []
+    return input_list[-5:]
 
 
 def middle(input_list):
@@ -88,7 +88,7 @@ def middle(input_list):
 
     """
 
-    return []
+    return input_list[2:-2]
 
 
 def inner_four(input_list):
@@ -101,7 +101,7 @@ def inner_four(input_list):
 
     """
 
-    return []
+    return input_list[2:6]
 
 
 def inner_four_end(input_list):
@@ -117,7 +117,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2]
 
 
 def replace_head(input_list):
@@ -134,7 +134,7 @@ def replace_head(input_list):
 
     """
 
-    pass
+    input_list[0] = 42
 
 
 def replace_third_and_last(input_list):
@@ -151,7 +151,7 @@ def replace_third_and_last(input_list):
 
     """
 
-    pass
+    input_list[2], input_list[-1] = 37, 37
 
 
 def replace_middle(input_list):
@@ -171,7 +171,8 @@ def replace_middle(input_list):
 
     """
 
-    pass
+    input_list[2:-2] = [42, 37]
+    
 
 
 def delete_third_and_seventh(input_list):
@@ -188,7 +189,9 @@ def delete_third_and_seventh(input_list):
 
     """
 
-    pass
+    del input_list[6]
+    del input_list[2]
+    
 
 
 def delete_middle(input_list):
@@ -207,4 +210,4 @@ def delete_middle(input_list):
 
     """
 
-    pass
+    input_list[2:-2] = []
